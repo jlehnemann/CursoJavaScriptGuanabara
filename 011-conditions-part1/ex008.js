503dee544acc20f8f725e51fd4d36246049d982c
Jose <j.lehnemann@gmail.com>
2026-09-16 21:39:08 -0300

    Added 011-conditions-part1

---
+++ 011-conditions-part1/ex008.js
@@ -0,0 +1,12 @@
+console.log('Olá, mundo!')
+
+var vel = 60.5
+
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if (vel > 60) {
+    console.log('Multado! Velocidade acima do permitido')
+}
+
+
+console.log('Dirija sempre com cinto de segurança')
