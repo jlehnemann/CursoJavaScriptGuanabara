1a51041fb4141fecdd5fd3ef396775fc94ad8352
Jose <j.lehnemann@gmail.com>
2026-09-22 21:32:01 -0300

    Added 016-loops-part1

---
+++ 016-loops-part1/ex016.js
@@ -0,0 +1,13 @@
+//loop - while
+var b = 1;
+while (b <= 6) {
+    console.log(`Passo ${b}`);
+    b++;
+}
+
+//loop - do while
+var c = 1;
+do {
+    console.log(`Passo ${c}`);
+    c++;
+} while (c <=6)
